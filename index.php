68bf2242b2e16a790a35498db53ac5d4d2e63503
berkbyrk <32482029+berkbyrk@users.noreply.github.com>
2018-03-12 20:47:02 +0300

    Update index.php
    nissan

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 		<a href='ogrencibilgi.php'>Ogrenci Bilgi</a><br>
 		<a href='derssecimi.php'>Ders Secimi</a><br>
 		<a href='bilgilerilistele.php'>Bilgileri Listele</a>
-	</td>
+	</td>Nissan 
 </tr>
 </table>
